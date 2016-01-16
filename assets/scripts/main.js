aa7edf0294fa8ee0ee563e79faa5c3bc9f7ffe61
Josh Rogan <jjjr1122@gmail.com>
2016-01-16 16:19:34 -0500

    Use angular list for primary layout. Remove dist files

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,31 @@
 var app = angular.module('BacApp', ['ngMaterial']).config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default').primaryPalette('blue-grey').accentPalette('light-blue');
 });
-
 //Main Controller
 app.controller('AppController', function() {
     var vm = this;
-    
 });
-
 //Actual Form to calculate value 
 app.controller('AppCtrl', function($scope) {
+    
+    //Setup Default Values
     $scope.data = {
-        food: 'None'
+        food: 'None', 
+        weight: 0, 
+        std_drinks: 0, 
+        duration: 0
     };
+
+    $scope.gender_options = [
+    	{ text: "Male", slug: 'male', extend: 'Tool tip? ' },
+    	{ text: "Female", slug: 'female', extend: 'Tool tip? ' },
+    ];
+
+    $scope.food_options = [
+    	{ text: "None", slug: 'none', extend: 'Tool tip? ' },
+    	{ text: "Small", slug: 'small', extend: 'Tool tip? ' },
+    	{ text: "Medium", slug: 'medium', extend: 'Tool tip? ' },
+    	{ text: "Full", slug: 'full', extend: 'Tool tip? ' }
+    ];
+
 });
